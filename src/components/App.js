6fdf2acdae028d2afbcfa55c5e7ff392e0afd235
Greg Answer <greganswer@gmail.com>
2017-09-05 16:11:00 -0400

    Desktop menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import 'materialize-css/dist/js/materialize';
+import $ from 'jquery/dist/jquery';
 import Header from './Header';
 
-class App extends Component {
+$(function() {
+  var $carousel;
+  $('.jsButtonCollapse').sideNav();
+  $('select').material_select();
+  $('.jsParallax').parallax();
+  $('.datepicker').pickadate({ selectMonths: true, selectYears: 20 });
+  $('.dropdown-button').dropdown({ constrainWidth: false, alignment: 'right' });
+  $('nav .dropdown-button').dropdown({ alignment: 'right', hover: true, belowOrigin: true });
+
+  $carousel = $('.carousel.carousel-slider');
+  $carousel.carousel({ fullWidth: true, indicators: true });
+  setInterval(() => {
+    return $carousel.carousel('next');
+  }, 6000);
+});
+
+export default class App extends Component {
   render() {
     return <Header />;
   }
 }
-
-export default App;
