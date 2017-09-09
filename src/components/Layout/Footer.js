1f7d5b80198ea74708bec17da60ad5acb1bd493a
Greg Answer <greganswer@gmail.com>
2017-09-09 00:56:42 -0400

    Add footer connection links

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -66,7 +66,50 @@ const Footer = props =>
             </div>
           </div>
         </div>
-        <div className="col m12 l6" />
+        <div className="col m12 l6">
+          <div className="col s12 m8 offset-m4">
+            <ul className="footer-social-links">
+              <li>
+                <a href="#">
+                  <i className="fa fa-facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-pinterest-p" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-linkedin-square" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-snapchat-ghost" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-youtube-play" />
+                </a>
+              </li>
+            </ul>
+            <div>
+              <a href="tel:18884980003">+1 888–498–0003</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
