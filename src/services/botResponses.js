b3f8f700d586a04b43302c884c66f3111503dc71
Greg Answer <greganswer@gmail.com>
2017-09-14 13:42:20 -0400

    Add authentication and error handling

--- src/services/botResponses.js
+++ src/services/botResponses.js
@@ -1,8 +1,25 @@
 import axios from 'axios';
+import chatMessageFormat from './chatMessageFormat';
 
 const CHATBOT_API_URL = process.env.REACT_APP_CHATBOT_API_URL;
 const CHATBOT_ACCESS_TOKEN = process.env.REACT_APP_CHATBOT_API_ACCESS_TOKEN;
-const CHATBOT_POST_URL = `${CHATBOT_API_URL}?access_token=${CHATBOT_ACCESS_TOKEN}`;
+const CHATBOT_POST_URL = `${CHATBOT_API_URL}/?access_token=${CHATBOT_ACCESS_TOKEN}`;
+
+/**
+ * [randomErrorMessages description]
+ * @return {[type]} [description]
+ */
+const randomErrorMessages = () => {
+  const random = Math.random();
+
+  if (random <= 0.33) {
+    return "⚠\nI'm sorry, I'm having trouble communicating with my server at the moment.\n\nPlease contact my developer and try again later.";
+  } else if (random >= 0.66) {
+    return "⚠\nUh oh! It looks like I'm not feeling well 😵 \n\nPlease let my creator know and try again later.";
+  }
+
+  return '⚠\nI seem to be under the weather right now 😷 \n\nPlease tell my creators so they can fix me up.';
+};
 
 /**
  * Process the user's input
@@ -13,10 +30,13 @@ async function botResponses(userInput) {
   try {
     return await axios.post(CHATBOT_POST_URL, { text: userInput });
   } catch (e) {
+    const data = e.response.data;
     console.log('Error retrieving bot responses');
-    console.log('e = ', JSON.stringify(e, null, 2));
+    console.log('data = ', JSON.stringify(data, null, 2));
 
-    return [];
+    return {
+      data: [chatMessageFormat(randomErrorMessages(), 'bot')],
+    };
   }
 }
 
--- src/services/chatMessageFormat.js
+++ src/services/chatMessageFormat.js
@@ -2,13 +2,16 @@ import uuid from 'uuid';
 
 /**
  * Process the user's text
- * @param  {string} text The user's userInput
+ * @param  {string} text The sender's message
+ * @param  {string} sender The the sender ot the message
  * @return {Object}
  */
-export default function chatMessageFormat(text) {
+export default function chatMessageFormat(text, sender = 'user') {
+  const recipient = sender === 'user' ? 'bot' : 'user';
+
   return {
-    sender: { type: 'user' },
-    recipient: { type: 'bot' },
+    sender: { type: sender },
+    recipient: { type: recipient },
     timestamp: Date.now(),
     message: {
       mid: uuid.v1(),
